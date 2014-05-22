52868a722913cdb049bf2d3b1abefdacb196f3c9
Sergey Fesyura <sergey.fesyura@dune-hd.com>
2014-05-22 13:24:18 +0300

    connect started

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-var WebSocket = require('ws')
-, ws = new WebSocket('ws://ws1.godville.net:82/hero');
+var WebSocket = require('ws');
+var ws = new WebSocket(
+	'ws://ws1.godville.net:82/hero',
+	undefined,
+	{
+		headers: {
+			Cookie:'god_session_id=2b5ce22933c940e78033538cf3e1a1b2; __utma=25621547.504172843.1400741251.1400741251.1400753701.2; __utmb=25621547.2.10.1400753701; __utmc=25621547; __utmz=25621547.1400741251.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)'
+		}
+	}
+);
 
 ws.on('open', function() {
 	console.log('4')
-	ws.send('subscribe');
+	ws.send('/fbh/feed?a=GjZLOZGK7ior8w2omUIsNhZ6Vcqm');
 });
 ws.on('message', function(message) {
 	console.log('7')
